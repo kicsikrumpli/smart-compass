7596e75de238e6f716342977e6b48a252b82f7b8
Daniel David Kovacs <v-dakovacs@expedia.com>
2018-05-22 13:29:06 +0200

    initial commit

---
+++ sketch_may18a/sketch_may18a.ino
@@ -0,0 +1,55 @@
+#include <Servo.h>
+#include <SoftwareSerial.h>
+#include <TinyGPS++.h>
+
+class Coord {
+  public:
+    long lat;
+    long lon;
+    Coord(long lat, long lon) : lat(lat), lon(lon) {};
+};
+
+class Model {
+  public:
+    Model(TinyGPSPlus& gps, Coord& destination):
+      gps(gps),
+      destination(destination) {}
+    // ToDo! facade methods???
+  private:
+    TinyGPSPlus& gps;
+    Coord& destination;
+};
+
+class Compass {
+  public:
+    Compass(Model& model, Servo& servo, SoftwareSerial& sserial):
+      model(model), 
+      sserial(sserial), 
+      servo(servo),
+      previousTick(0) {}
+    void update();
+  private:
+    const unsigned long TEST_INTERVAL = 1000;
+    Model& model;
+    Servo& servo;
+    SoftwareSerial& sserial;
+    unsigned long previousTick;
+};
+
+void Compass::update() {
+  unsigned long tick = millis();
+  if (previousTick - tick > TEST_INTERVAL) {
+    previousTick = tick;
+  }
+  return;
+}
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
